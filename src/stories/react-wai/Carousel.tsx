305994bf9b5f040b8231c1599a8d75150ded378c
codevil <namhundred@naver.com>
2023-02-12 22:06:19 +0900

    feat: add carousel page option

--- src/stories/react-wai/Carousel.tsx
+++ src/stories/react-wai/Carousel.tsx
@@ -3,20 +3,34 @@ import styled from "@emotion/styled";
 
 interface IPages {
   children: JSX.Element | JSX.Element[];
+  current: number;
+  n: number;
 }
 const Container = styled.section`
-  max-width: 900px;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
 `;
 const Tab = styled.div``;
-const Pages = styled.ul<IPages>``;
-function Controller() {
-  return (
-    <>
-      <button>Stop/Play</button>
-      <Tab></Tab>
-    </>
+const Pages = styled.ul<IPages>`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  width: ${({ n }) => `${100 * n}%`};
+  transform: translate3d(
+    ${({ current, n }) => `${(current * 100) / n}%`},
+    0,
+    0
   );
-}
+  flex-direction: row;
+  & * {
+    width: 100%;
+    list-style: none;
+  }
+`;
+const Controller = styled.div`
+  position: absolute;
+`;
 
 export function Carousel({
   name,
@@ -25,10 +39,19 @@ export function Carousel({
   name?: string;
   children: JSX.Element | JSX.Element[];
 }) {
+  let current = 1;
   return (
-    <Container aria-label={name}>
-      <Controller />
-      <Pages>{children}</Pages>
+    <Container aria-roledescription="carousel" aria-label={name}>
+      <Controller>
+        <button aria-label="Stop automatic slide show">Stop/Play</button>
+        <Tab></Tab>
+      </Controller>
+      <Pages
+        current={current}
+        n={Array.isArray(children) ? children.length : 1}
+      >
+        {children}
+      </Pages>
     </Container>
   );
 }
